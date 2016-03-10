6563bbbab4e04d74700cce5b00c8f41f1c3c8c56
Maciej Kwiecien <maciej.kwiecien@gmail.com>
2016-03-10 14:24:33 +0100

    Played a bit with Externalized Configuration, banner and custom MANIFEST.MF entries

--- livelessons-configuration/livelessons-configuration-basic/src/main/java/demo/Application.java
+++ livelessons-configuration/livelessons-configuration-basic/src/main/java/demo/Application.java
@@ -13,25 +13,30 @@ import org.springframework.core.env.Environment;
 @EnableConfigurationProperties
 public class Application {
 
-	@Value("${configuration.projectName}")
-	void setProjectName(String projectName) {
-		System.out.println("setting project name: " + projectName);
-	}
+    @Value("${configuration.projectName}")
+    void setProjectName(String projectName) {
+        System.out.println("setting project name: " + projectName);
+    }
 
-	@Autowired
-	void setEnvironment(Environment env) {
-		System.out.println("setting environment: "
-				+ env.getProperty("configuration.projectName"));
-	}
+    @Autowired
+    void setEnvironment(Environment env) {
+        System.out.println("setting environment: "
+                + env.getProperty("configuration.projectName"));
+    }
 
-	@Autowired
-	void setConfigurationProjectProperties(ConfigurationProjectProperties cp) {
-		System.out.println("configurationProjectProperties.projectName = "
-				+ cp.getProjectName());
-	}
+    @Autowired
+    void setConfigurationProjectProperties(ConfigurationProjectProperties cp) {
+        System.out.println("configurationProjectProperties.projectName = "
+                + cp.getProjectName());
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class);
-	}
+    @Value("${foo}")
+    void setFoo(String foo) {
+        System.out.println("property 'foo' :" + foo);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
 
 }
